3e5a8ee4bd699be19ef15ca2ecf97489f48908ce
ch.gopi <ch.gopi7893@gmail.com>
2024-04-08 14:14:52 +0530

    service

--- src/main/java/com/example/Employee/controller/EmployeeController.java
+++ src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -1,26 +1,23 @@
 package com.example.Employee.controller;
-
 import com.example.Employee.Entity.Employee;
+import com.example.Employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
+
 
 @Controller
 @ResponseBody
 //@RestController
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
     @RequestMapping("/employees")
      public List<Employee> findallemployees(){
-         List<Employee> EmployeeList= Arrays.asList(
-                 new Employee(1,"Gopi","HYDERABAD"),
-                 new Employee(2,"Muneer","HYDERABAD")
 
-         );
-
-         return EmployeeList;
+         return employeeService.getAllEmployees();
     }
 }
