6d3b80ed05e2d7b021e6afe5f68993eb727b58cb
ch.gopi <ch.gopi7893@gmail.com>
2024-04-08 23:37:17 +0530

    CRUD operations

--- src/main/java/com/example/Employee/service/EmployeeService.java
+++ src/main/java/com/example/Employee/service/EmployeeService.java
@@ -2,17 +2,21 @@ package com.example.Employee.service;
 
 import com.example.Employee.Entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    List<Employee> employeeList= Arrays.asList(
+    List<Employee> employeeList=new ArrayList<>( Arrays.asList(
             new Employee(1,"Gopi","HYDERABAD"),
             new Employee(2,"Muneer","HYDERABAD")
 
-    );
+    ));
 
     public  List<Employee> getAllEmployees()
     {
@@ -20,4 +24,29 @@ public class EmployeeService {
     }
     public Employee getAnEmployee(int id){
         return  employeeList.stream().filter(e -> (e.getEmployeeId()== id)).findFirst().get();
-    }}
+    }
+    public  void createEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+    public void updateEmployee(Employee employee){
+        List<Employee> tempEmployee = new ArrayList<>();
+        for(Employee emp:employeeList){
+            if (emp.getEmployeeId()==employee.getEmployeeId()){
+                emp.setEmployeeName(employee.getEmployeeName());
+                emp.setEmployeeCity(employee.getEmployeeCity());
+            }
+            tempEmployee.add(emp);
+        }
+        this.employeeList=tempEmployee;
+
+    }
+    public void deleteEmployee(int id){
+        ArrayList <Employee> tempEmployee  = new ArrayList<>();
+        for(Employee emp:employeeList) {
+            if (emp.getEmployeeId() == id)
+                continue;
+            tempEmployee.add(emp);
+        }
+            this.employeeList=tempEmployee;
+    }
+}
